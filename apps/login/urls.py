e2a8d2df7ea5293d5cf1390a322caab1ce6929c4
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-20 21:07:05 -0500

    subir11

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from apps.login.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path(r'', index),
-]
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
