b51e44f90d9125c1f5fe64b3daa501cc3886d76b
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-21 16:03:51 -0500

    font4

--- examen/urls.py
+++ examen/urls.py
@@ -13,14 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from examen.settings import STATICFILES_STORAGE
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.views import LoginView, logout_then_login
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
+    path('favicon.ico',RedirectView.as_view(url=staticfiles_storage.url('img/favicon.png'))),
     path('admin/', admin.site.urls),
     path('empresa/', include ('apps.empresa.urls')),
     path('departamento/', include ('apps.departamento.urls')),
